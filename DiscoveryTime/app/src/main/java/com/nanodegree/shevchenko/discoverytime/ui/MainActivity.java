8bec1c0ea83852cc654ed05a603fe9cc19a0c1ba
Anastasiya Shevchenko <nastjashevchenko@gmail.com>
2016-04-21 17:07:53 -0700

    Feat: connection to Google Places API

--- DiscoveryTime/app/src/main/java/com/nanodegree/shevchenko/discoverytime/ui/MainActivity.java
+++ DiscoveryTime/app/src/main/java/com/nanodegree/shevchenko/discoverytime/ui/MainActivity.java
@@ -1,22 +1,54 @@
 package com.nanodegree.shevchenko.discoverytime.ui;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.places.Places;
 import com.nanodegree.shevchenko.discoverytime.R;
 import com.nanodegree.shevchenko.discoverytime.adapters.TripListPagerAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements OnConnectionFailedListener {
+    private GoogleApiClient mGoogleApiClient;
+    private static final String LOG_TAG = MainActivity.class.getName();;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .enableAutoManage(this, this)
+                .build();
+
+        /* Test sample to test Google Places API connection
+        String placeId = "ChIJ-6BSGSeG3EMRllG2gblbMBU";
+        Places.GeoDataApi.getPlaceById(mGoogleApiClient, placeId)
+                .setResultCallback(new ResultCallback<PlaceBuffer>() {
+                    @Override
+                    public void onResult(PlaceBuffer places) {
+                        if (places.getStatus().isSuccess() && places.getCount() > 0) {
+                            final Place myPlace = places.get(0);
+                            Log.i(LOG_TAG, "Place found: " + myPlace.getName());
+                        } else {
+                            Log.e(LOG_TAG, "Place not found");
+                        }
+                        places.release();
+                    }
+                }); */
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -48,5 +80,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(LOG_TAG, connectionResult.getErrorMessage());
     }
 }
